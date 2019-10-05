af07708bded2963fcf91d93f16ba0b4df7f44e07
changqing627 <2621008548@qq.com>
2019-10-05 10:56:30 +0800

    comple

--- script/index.js
+++ script/index.js
@@ -1,5 +1,54 @@
 $(function (){
-    //头尾
-    $(".head").load("head.html");
-    $(".footer").load("footer.html"); 
+
+// head footer 适配
+var winH=$(window).height();
+var contH=$(document.body).height();
+var allContH=contH+142+296;
+var footTop=winH-allContH;
+if(winH>allContH){
+  $(".footer").css("marginTop",footTop+"px");
+};
+
+// head nav 当前选中颜色
+function headerNav(){
+  var navName=$("meta[name=headerName]").attr("content");
+  $(".headUl li").each(function (){
+    var _this=$(this);
+    if(navName==_this.attr("data-name")){
+        _this.find("a").addClass("active")
+    }
+      
+  })
+};
+headerNav();
+//  small nav
+$(window).bind("scroll",function (){
+  var sTop=$(this).scrollTop();
+  // console.log(sTop);
+  if(sTop>=200){
+    $(".smailNav").css("display","block")
+  }else{
+    $(".smailNav").css("display","none")
+  }
+  
 })
+
+//  right 
+
+var windWid=$(window).width();
+var rigLeft=(windWid-1080)/2+1080-30;
+$(".rigBox").css("marginLeft",rigLeft+"px");
+var rigLi=$(".rigBox ul li");
+rigLi.each(function (){
+  $(this).mouseover(function (){
+    $(this).find("a img").css("display","block")
+  }).mouseout(function (){
+    $(this).find("a img").css("display","none")
+  })
+})
+  
+
+
+
+})
+
